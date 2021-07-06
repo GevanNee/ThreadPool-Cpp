04732d1d0820c154035619cbc6ba072b896d314e
GevanNee <284440688@qq.com>
2021-07-07 02:46:52 +0800

    commit

--- VS2017Project/threadPoolCPP/threadPoolCPP/threadpool.cpp
+++ VS2017Project/threadPoolCPP/threadPoolCPP/threadpool.cpp
@@ -1,10 +1,110 @@
-#include "threadpool.h"
- 
- class threadPool
- {
-    private:
-    struct task
-    {
-        
-    };
- };
+﻿#include "threadpool.h"
+#include <iostream>
+#include <string.h>
+ #ifndef DEBUG
+ #define DEBUG 1
+ #endif
+using namespace std;
+void* threadPool::manager(threadPool* arg)
+{
+
+}
+void* threadPool::worker(threadPool* arg)
+{
+	
+	while (1)
+	{
+		pthread_mutex_lock(&mutexPool);
+		//当任务队列为空的时候需要一直判断
+		while (Front == Rear && false == shutdown)
+		{
+			pthread_cond_wait(&notEmpty, &mutexPool);
+			if (exitNum > 0)
+			{
+				exitNum--;
+				if (threadLiveNum > minNum)
+				{
+					cout << "thread" << pthread_self() << "exiting becase exitNum > 0" << endl;
+					exitThread();
+				}
+			}
+		}
+		if (shutdown)
+		{
+			cout << "threadID:" << pthread_self() << "exiting becase shutdown == true" << endl;
+			exitThread();
+
+		}
+
+
+		pthread_mutex_lock(&mutexPool);
+	}
+	
+	
+}
+static threadPool* createPool( int max, int min, int taskQueueSize)
+{
+	threadPool* pool = new threadPool(max, min, taskQueueSize);
+	return pool;
+}
+
+//构造函数,
+threadPool::threadPool(int max, int min, int taskQueueSize)
+{
+	if (0 == ThreadPoolInit(max, min, taskQueueSize))
+	{
+		std::cout << "init pool fail..." << std::endl;
+	}
+}
+
+
+/*辅助函数*/
+int threadPool::ThreadPoolInit(int max, int min, int taskQueueSize)
+{
+	do {
+		/*初始化taskQ队列， */
+		TaskQ = new std::vector<Task>;
+		if (nullptr == TaskQ)
+		{
+			std::cout << "new TaskQ fail..." << std::endl;
+			break;
+		}
+		for (int i = 0; i < taskQueueSize; i++)
+		{
+			Task temp = Task{ nullptr, nullptr };
+			TaskQ->push_back(temp);
+		}
+		Front =  Rear = TaskQ->begin();
+		/*初始化taskQ完毕 */
+
+		pthread_create(&managerID, nullptr, this->manager, this);
+		workerIDs = new pthread_t[max];
+		if (nullptr == workerIDs)
+		{
+			std::cout << "new workerID fail..." << std::endl;
+			break;
+		}
+		for (int i = 0; i < max; i++)
+		{
+			pthread_create(&workerIDs[i], nullptr, this->worker, this);
+		}
+		threadBusyNum = 0;//线程池中忙线程的数量，这个值一直在变需要高频访问，所以单独用个锁
+		threadLiveNum = min;
+		maxNum = max;
+		minNum = min;
+		exitNum = 0;
+		if (pthread_mutex_init(&mutexPool, nullptr) != 0 ||
+			pthread_mutex_init(&mutexBusy, nullptr) != 0 ||
+			pthread_cond_init(&notEmpty, nullptr) != 0 ||
+			pthread_cond_init(&notFull, nullptr) != 0)
+		{
+			std::cout << "mutex or condition init fail..." << std::endl;
+			break;
+		}
+		shutdown = false;
+		return 1;
+	} while (0);
+	if (TaskQ) delete TaskQ;
+	if (workerIDs) delete[] workerIDs;
+	return 0;
+}
--- VS2017Project/threadPoolCPP/threadPoolCPP/threadpool.h
+++ VS2017Project/threadPoolCPP/threadPoolCPP/threadpool.h
@@ -1,1 +1,51 @@
-#pragma once
+﻿#pragma once
+#include "pthread.h"
+#include <queue>
+class threadPool
+ {
+private:
+	//单个任务
+    typedef struct Task
+    {
+        void (*func)(void* arg);
+        void* arg;
+    }Task;
+	//任务组成的队列
+	
+
+public:
+    int ThreadPooldestory(threadPool* pool);
+    void addTask(void (*func), void* arg);
+public:
+    int LiveThreadNum(threadPool* pool);
+    int TaskNum(threadPool* pool);
+	static threadPool* createPool(threadPool&pool, int max, int min, int taskQueueSize);
+	threadPool(int max, int min, int taskQueueSize);
+	threadPool();
+private://辅助函数
+	~threadPool();
+	void exitThread();
+	int ThreadPoolInit(int max, int min, int taskQueueSize);
+	void* manager(threadPool* arg);
+	void* worker(threadPool* arg);
+ private://Task队列，用vector和两个迭代器实现不删除元素的循环队列，也可以用循环链表
+	std::vector<Task>* TaskQ;
+	std::vector<Task>::iterator Front;
+	std::vector<Task>::iterator Rear;
+ private://工作线程和管理者线程的ID
+	pthread_t managerID;
+	pthread_t* workerIDs;
+ private://线程池的属性变量
+	int threadBusyNum;//线程池中的忙线程数量
+	int threadLiveNum;//线程池中存活线程的数量
+	int maxNum;//线程池中最大线程数量
+	int minNum;//线程池中最小线程数量
+	int exitNum;//线程池中需要销毁的线程的数量
+	bool shutdown;//判断线程是否应该被销毁
+ private://线程池中的锁
+	pthread_mutex_t mutexPool;//整个池子的锁
+	pthread_mutex_t mutexBusy;//变量 threadBusyNum 的专属锁，因为该变量访问频率很高
+	pthread_cond_t notEmpty;//该条件变量用来唤醒生产者(addTask函数)
+	pthread_cond_t notFull;//该条件变量用来唤醒消费者(worker函数)
+	
+};
